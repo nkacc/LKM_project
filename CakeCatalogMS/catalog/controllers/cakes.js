8c7c966ca4c28eca83982ccb9c2eec3fe8580e44
Naveen S R <naveen.s.r@accenture.com>
2022-11-30 13:54:18 +0530

    CakeCatalog

--- CakeCatalogMS/catalog/controllers/cakes.js
+++ CakeCatalogMS/catalog/controllers/cakes.js
@@ -6,17 +6,40 @@ function getallcakes(req, res) {
     )
 }
 
-function getcakesbyname() {}
+function getcakesbyname(req, res) {
+    Cakes.find({name:req.params.name}).then(
+        docs => res.send(docs)
+    )
+}
 
-function getcakesbyprice() {}
+function getcakesbyprice(min, max, res) {
+    Cakes.find({
+        price: {$gte: min, $lte: max}
+    }).then(
+        docs => res.send(docs)
+    )
+}
 
-function getcakesbyprice_param() {}
+function getcakesbyprice_param(req, res) {
+    getcakesbyprice(
+        req.params.min,
+        req.params.max,
+        res
+    )
+}
 
-function getcakesbyprice_query() {}
+function getcakesbyprice_query(req, res) {
+    getcakesbyprice(
+        req.query.min,
+        req.query.max,
+        res
+    )
+}
 
 
 module.exports = {
     getallcakes,
     getcakesbyname,
-    getcakesbyprice
+    getcakesbyprice_param,
+    getcakesbyprice_query
 }
--- CakeCatalogMS/catalog/routes/cakes.js
+++ CakeCatalogMS/catalog/routes/cakes.js
@@ -3,7 +3,13 @@ var express = require("express");
 const cakes_controller = require("../controllers/cakes");
 
 var router = express.Router();
-router.get("/", cakes_controller.getallcakes);
+router.get("/getallcakes", cakes_controller.getallcakes)
+router.get("/getcakesbyname/:name", cakes_controller.getcakesbyname)
 
+var getcakesbyprice_router = express.Router()
+getcakesbyprice_router.get("/:min/:max", cakes_controller.getcakesbyprice_param)
+getcakesbyprice_router.get("/", cakes_controller.getcakesbyprice_query)
+
+router.use("/getcakesbyprice", getcakesbyprice_router)
 
 module.exports = router;
