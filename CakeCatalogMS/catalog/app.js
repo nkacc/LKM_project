1dd3d0836a57e8d39930bf60a2d5aba6932bcaff
Naveen S R <naveen.s.r@accenture.com>
2022-11-30 11:48:16 +0530

    typos

--- CakeCatalogMS/catalog/app.js
+++ CakeCatalogMS/catalog/app.js
@@ -6,11 +6,12 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var cakesRouter = require('./routes/cakes')
 
 var app = express();
 
 // Mongoose connection
-require('../db/dbconfig')
+require('./db/dbconfig')
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/cakes', cakesRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
